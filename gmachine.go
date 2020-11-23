04aef181f902fbc37f4888e4c192077dec45665a
basebandit <evansonmwangi83@gmail.com>
2020-11-23 18:22:37 +0300

    Task 1: Create a GMachine struct with a default constructor method

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,20 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+//GMachine is a state-machine representing a virtual CPU.
+type GMachine struct {
+	//P is a 64 bit register. Used to store the memory address of the next instruction to execute.
+	P uint64
+	//Memory is an array of 64 bit words that represent the memory of a G-Machine.
+	Memory []uint64
+}
+
+//New initializes a GMachine instance to its default state. Returns a pointer to a G-Machine instance.
+func New() *GMachine {
+	gm := GMachine{
+		Memory: make([]uint64, DefaultMemSize),
+	}
+
+	return &gm
+}
